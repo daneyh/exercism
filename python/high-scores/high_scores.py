fdbda3cbae1258886ef56d8ce6b8395e1d40c440
Dane Harris <enadsirrah@gmail.com>
2019-12-25 23:04:06 +0000

    high scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,15 @@
 def latest(scores):
-    pass
+   return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    top_three = []
+    while len(top_three) < 3 and len(scores) > 0:
+        top_three.append(max(scores))
+        scores.remove(max(scores))
+        
+    return top_three
