eae5155080656a10513c898966e59b475de7c35a
Dane Harris <enadsirrah@gmail.com>
2019-12-29 23:02:46 +0000

    Completed hamming test

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,14 @@
 def distance(strand_a, strand_b):
-    pass
+    distance = 0
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands are of two different lengths")
+    else:
+        for index in range(len(strand_a)):
+            if strand_a[index] != strand_b[index]:
+                distance += 1
+    return distance
+
+#strand_a = 'ABCDABCDABCDABCD'
+#strand_b = 'DBCAABADACCDDBCD'
+#5 diffs     ^  ^  ^  ^  ^
+
